f22a1602d8d9201e0e4c388faff5c80c1f16be91
AidaNu2021 <aida.nusinovic@gmail.com>
2021-03-16 01:03:23 +0100

    dodat ex14

---
+++ ex14/src/index.js
@@ -0,0 +1,15 @@
+
+function convertStrToInt(str) {
+
+var convertStrToInt = parseInt("44");
+var convertStrToInt = parseInt("44");
+var convertStrToInt = parseInt("27");
+var convertStrToInt = parseInt("JeffBezos");
+}
+console.log(parseInt("44"));
+console.log(parseInt("44"));
+console.log(parseInt("27"));
+console.log(parseInt("JeffBezos"));
+
+
+module.exports = convertStrToInt;
